cd14765c75fde3fa224db921980473f5a00e97de
Claudio Ezequiel Jara <claudiojara848@gmail.com>
2021-05-29 11:45:16 -0300

    Funcionalidad backend: borar y modificar imagenes en cloudinary

--- backend/api/imagen.route.js
+++ backend/api/imagen.route.js
@@ -1,11 +1,7 @@
 import express from "express"
-import dotenv from "dotenv"
 import cloudinary from "../utils/cloudinary.config.js";
 import multer from "../utils/multer.js";
 
-//import User, { find, findById, findByIdAndUpdate } from "../model/user";
-
-dotenv.config()
 const router = express.Router()
 
 cloudinary.v2.config({
@@ -32,6 +28,44 @@ router.post("/", multer.single("imagen"), async (req, res) => {
   }
 });
 
+router.delete("/", async (req, res) => {
+  try {
+
+    // Delete image from cloudinary
+    await cloudinary.v2.uploader.destroy(req.query.id);
+
+    res.json({ status: "Eliminado" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.put("/:id", multer.single("imagen"), async (req, res) => {
+  try {
+    //console.log(req.file, req.params.id);
+    // Delete image from cloudinary
+    await cloudinary.v2.uploader.destroy(req.params.id);
+    // Upload image to cloudinary
+    let result;
+    if (req.file) {
+      result = await cloudinary.v2.uploader.upload(req.file.path);
+    }
+    /*
+    const imagen = {
+      url: result?.secure_url || user.avatar,
+      id: result?.public_id || user.cloudinary_id,
+    };
+    */
+    const imagen = {
+      url: result.secure_url,
+      id: result.public_id,
+    };
+    res.json(imagen);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 /*
 router.get("/", async (req, res) => {
   try {
@@ -42,41 +76,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.delete("/:id", async (req, res) => {
-  try {
-    // Find user by id
-    let user = await findById(req.params.id);
-    // Delete image from cloudinary
-    await uploader.destroy(user.cloudinary_id);
-    // Delete user from db
-    await user.remove();
-    res.json(user);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-router.put("/:id", single("image"), async (req, res) => {
-  try {
-    let user = await findById(req.params.id);
-    // Delete image from cloudinary
-    await uploader.destroy(user.cloudinary_id);
-    // Upload image to cloudinary
-    let result;
-    if (req.file) {
-      result = await uploader.upload(req.file.path);
-    }
-    const data = {
-      name: req.body.name || user.name,
-      avatar: result?.secure_url || user.avatar,
-      cloudinary_id: result?.public_id || user.cloudinary_id,
-    };
-    user = await findByIdAndUpdate(req.params.id, data, { new: true });
-    res.json(user);
-  } catch (err) {
-    console.log(err);
-  }
-});
 
 router.get("/:id", async (req, res) => {
   try {
